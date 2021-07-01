abe5426a8fe6bd59da1644e261a9bea69ad669e0
Chinguun Erdenebileg <chiig.erdenebileg@gmail.com>
2021-07-02 00:50:54 +0800

    Color selector

--- gui/app.py
+++ gui/app.py
@@ -1,27 +1,41 @@
 from flask import Flask, render_template, request, session
+from flask_session import Session
 from markupsafe import escape
-import os, math
+import os
+import math
 
-TEMPLATE_DIR = os.path.abspath('../templates')
-STATIC_DIR = os.path.abspath('../static')
+TEMPLATE_DIR = os.path.abspath('templates')
+STATIC_DIR = os.path.abspath('static')
 
-# app = Flask(__name__) # to make the app run without any
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
-#this is a comment
+
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-#session is a dict unique to you.
+Session(app)
 
 
 @app.route("/", methods = ["GET", "POST"])
 def home():
     if request.method == "GET":
-        return render_template("index.html", answer = "")
+        if session.get('backgroundColor') != None:
+            return render_template("index.html", backgroundColor = session['backgroundColor'])
+        else:
+            return render_template("index.html", backgroundColor = "steelblue")
+    elif request.method == "POST":
+        session['backgroundColor'] = request.form.get("colorOptions")
+        return render_template("index.html", backgroundColor = session['backgroundColor'])
+
+@app.route("/math", methods = ["GET", "POST"])
+def calcSqrt():
+    if request.method == "GET":
+        if session.get('backgroundColor') != None:
+            return render_template("index.html", backgroundColor = session['backgroundColor'])
+        else:
+            return render_template("index.html", backgroundColor = "steelblue")
     elif request.method == "POST":
         answerNum = math.sqrt(int(request.form.get("name")))
-        return render_template("index.html", answer = f"{answerNum:0.2f}")
+        return render_template("index.html", answer = f"{answerNum:10.2f}", backgroundColor = session['backgroundColor'])
 
-@app.route("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
