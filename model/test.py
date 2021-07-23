d5706be869fe4c38430a4903c1c33b366c254ce6
apmalinsky <apmalinsky@gmail.com>
2021-07-23 10:32:21 -0400

    tester for storage

--- model/test.py
+++ model/test.py
@@ -4,21 +4,30 @@ from example_networks import *
 from model_storage import *
 
 
-net = AND(32)
+net1 = AND(4)
+net2 = OR(4)
 
-a = torch.tensor([1.])
+a = torch.tensor([0.])
 b = torch.tensor([1.])
-print(net(a, b))
+print(net1(a,b))
+print(net2(a,b))
+
 
 # save model
-saveModel(net)
+saveModel(net1)
+saveModel(net2)
 
 # load model
-model_dict = loadModel(net.__class__.__name__)
-model = model_dict['class']
-model.load_state_dict(model_dict['state_dict'])
+model_dict = loadModel(net1.__class__.__name__)
+model1 = model_dict['class']
+model1.load_state_dict(model_dict['state_dict'])
 
-print(model(a, b))
+model_dict = loadModel(net2.__class__.__name__)
+model2 = model_dict['class']
+model2.load_state_dict(model_dict['state_dict'])
+
+print(model1(a,b))
+print(model2(a,b))
 
 
 
